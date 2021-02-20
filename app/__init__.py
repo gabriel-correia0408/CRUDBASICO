689026e024fef9f32deeeb5316a6ec97329b9062
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-02-20 15:31:21 -0300

    adicioando classe que faz a serealização dos dados com o marshmelow e adicionado no init do app a configuração para fazer essa serialização quando inicializar o sitema

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
+
 from app.model import configure as config_db, db
+from .sererializer import configure as config_ma
 
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\gabriel.correia\\Desktop\\CRUDBASICO\\crudebasico.db'
     app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     config_db(app)
+    config_ma(app)
 
     Migrate(app, db)
     return app
--- app/sererializer.py
+++ app/sererializer.py
@@ -1,7 +1,14 @@
 from flask_marshmallow import Marshmallow
 
+from app.model import Book
+
 ma = Marshmallow()
 
 
 def configure(app):
     ma.init_app(app)
+
+
+class BookSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
