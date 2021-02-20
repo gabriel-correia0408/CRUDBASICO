4345be1aefa02caf1bcc5ebaa7501fb8aa6f2ee9
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-02-20 15:55:41 -0300

    adicionado mais três rotas, para assim formar as quatros rotas necessárias para se ter um crud, no caso, deletar, mostrar, modificar e cadastrar

--- app/books.py
+++ app/books.py
@@ -6,3 +6,18 @@ bp_books = Blueprint('books', __name__)
 @bp_books.route('/mostrar', methods=['GET'])
 def mostrar():
     ...
+
+
+@bp_books.route('/deletar', methods=['DELETE'])
+def deletar():
+    ...
+
+
+@bp_books.route('/modificar', methods=['PATCH'])
+def modificar():
+    ...
+
+
+@bp_books.route('/cadastrar', methods=['PUT'])
+def cadastar():
+    ...
