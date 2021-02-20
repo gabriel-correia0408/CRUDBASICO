37e2c43671ec01fc80956c9338832136e68b9590
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-02-20 15:09:31 -0300

    adicionando linha de configuração para retira de erro na hora de fazer um upfrade para o banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.model import configure as config_db, db
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\gabriel.correia\\Desktop\\CRUDBASICO\\crudebasico.db'
+    app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
     config_db(app)
 
     Migrate(app, db)
