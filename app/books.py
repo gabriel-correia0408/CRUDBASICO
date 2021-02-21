9a7a5ff73ff5d8d6837ad878a5235399ef9d108b
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-02-21 16:24:32 -0300

    retirada de código que teve sua abordagem mudada pois não o método usado não apresentava consistencia em sua abordagem

--- app/books.py
+++ app/books.py
@@ -22,12 +22,3 @@ def deletar():
 @bp_books.route('/modificar', methods=['PATCH'])
 def modificar():
     ...
-
-
-@bp_books.route('/cadastrar', methods=['POST'])
-def cadastar():
-    bs = BookSchema()
-    book, error = bs.load(request.json)
-    current_app.db.session.add(book)
-    current_app.db.session.commit()
-    return bs.jsonify(book), 201
